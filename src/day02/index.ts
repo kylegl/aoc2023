c3ff279ee013ee1ad004803b4bbbab22ad6b51cf
Kyle <lauber.kyle@gmail.com>
2023-12-06 15:43:33 -0800

    feat: day 2 part 1

---
+++ src/day02/index.ts
@@ -0,0 +1,86 @@
+import run from "aocrunner"
+
+const parseInput = (rawInput: string) => rawInput
+
+const cubes = {
+  red: 12,
+  green: 13,
+  blue: 14,
+}
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput)
+
+  const games = input.split('\n')
+
+  const possibleGameIds = []
+
+  gameloop: for (let i = 0; i < games.length; i++) {
+    const g = games[i]
+
+    const [header, rawRounds] = g.split(': ') 
+  
+    // get games # of pulled types cubes
+    const rounds = rawRounds.split('; ')
+
+    for (let j = 0; j < rounds.length; j++) {
+      const r = rounds[j]
+
+      const gameTotals = r.split(', ').reduce((total, rawCubes) => {
+        const [num, type] = rawCubes.split(' ')
+
+        total[type as keyof typeof total] = parseInt(num)
+        total.total += parseInt(num)
+
+        return total
+      }, { red: 0, green: 0, blue: 0, total: 0 })
+      
+      if (gameTotals.total > 39) {
+        continue gameloop
+      }
+
+      const gameTotalsArr = Object.entries(gameTotals)
+
+      for (let k = 0; k < gameTotalsArr.length; k++) {
+        const [type, total] = gameTotalsArr[k]
+
+        if (total > cubes[type as keyof typeof cubes]) {
+          continue gameloop
+        }
+      }
+    }
+
+    possibleGameIds.push(parseInt(header.split(' ')[1]))
+  }
+
+  return possibleGameIds.reduce((total, id) => total + id, 0)
+}
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput)
+
+  return
+}
+
+run({
+  part1: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+})
