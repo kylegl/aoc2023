d33e97d49b2219e68b1a44b6eefc4c34bf000b62
Kyle <lauber.kyle@gmail.com>
2023-12-05 17:12:45 -0800

    feat: day 1

--- src/day01/index.ts
+++ src/day01/index.ts
@@ -4,14 +4,67 @@ const parseInput = (rawInput: string) => rawInput
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput)
+  const lines = input.split('\n')
 
-  return
+  const parsedCalibrationCodes = lines.map(l => {
+    const numChars = l.split('').filter(c => (/[0-9]/.test(c)))
+
+    return numChars.length 
+      ? numChars.length === 1
+        ? parseInt(numChars[0] + numChars[0])
+        : parseInt(numChars[0] + numChars.pop())
+      : 0
+  })
+
+  return parsedCalibrationCodes.reduce((a, b) => a + b, 0)
 }
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput)
 
-  return
+  const lines = input.split('\n')
+
+  const parsedCalibrationCodes = lines.map((l, i) => {
+    const numTypeMatches = Array.from(
+      l.matchAll(/(?=(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)|(\d))/g),
+      (m) => m.filter(Boolean)
+    ).flat()
+
+
+    if (!numTypeMatches || !numTypeMatches.length)
+      return 0
+
+    const firstNumStr = convertNumWordToNumStr(numTypeMatches[0])
+
+    return numTypeMatches.length === 1
+        ? parseInt(firstNumStr + firstNumStr)
+        : parseInt(firstNumStr + convertNumWordToNumStr(numTypeMatches.pop()))
+  })
+
+  return parsedCalibrationCodes.reduce((a, b) => a + b, 0)
+}
+
+/// HELPERS 
+const numWordMap = {
+  one: '1',
+  two: '2',
+  three: '3',
+  four: '4',
+  five: '5',
+  six: '6',
+  seven: '7',
+  eight: '8',
+  nine: '9',
+}
+
+function convertNumWordToNumStr(str?: string) {
+  if (!str)
+    return ''
+
+  if (/[0-9]/.test(str))
+    return str
+  
+  return numWordMap[str as keyof typeof numWordMap]
 }
 
 run({
