ad1f9c9c74e75010717a2f25b1bebae29198db89
Kyle <lauber.kyle@gmail.com>
2023-12-06 17:16:19 -0800

    feat: day 2 part 2

--- src/day02/index.ts
+++ src/day02/index.ts
@@ -58,8 +58,50 @@ const part1 = (rawInput: string) => {
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput)
+    const games = input.split('\n')
 
-  return
+  const gamePowers = []
+
+  gameloop: for (let i = 0; i < games.length; i++) {
+    const g = games[i]
+
+    const max = {
+      red: 0,
+      green: 0,
+      blue: 0,
+    }
+
+    const [, rawRounds] = g.split(': ') 
+  
+    const rounds = rawRounds.split('; ')
+
+    for (let j = 0; j < rounds.length; j++) {
+      const r = rounds[j]
+
+      const gameTotals = r.split(', ').reduce((total, rawCubes) => {
+        const [num, type] = rawCubes.split(' ')
+
+        total[type as keyof typeof total] = parseInt(num)
+        total.total += parseInt(num)
+
+        return total
+      }, { red: 0, green: 0, blue: 0, total: 0 })
+      
+      const gameTotalsArr = Object.entries(gameTotals)
+
+      for (let k = 0; k < gameTotalsArr.length; k++) {
+        const [type, total] = gameTotalsArr[k]
+
+        if (total > max[type as keyof typeof cubes]) {
+          max[type as keyof typeof cubes] = total
+        }
+      }
+
+    }
+    gamePowers.push(Object.values(max).reduce((a, b) => a * b, 1))
+  }
+
+  return gamePowers.reduce((total, power) => total + power, 0)
 }
 
 run({
